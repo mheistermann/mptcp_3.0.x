9c1e286df9d54f2ea27e9d7cffc4159de5a94b47
Huisung Kang <hs1218.kang@samsung.com>
2011-12-08 21:10:48 +0900

    ARM: SAMSUNG: Add EXYNOS5250 revision information
    Change-Id: I9356190c45b0320275e9cfa4a3e3c70c14db39f8
    Signed-off-by: Huisung Kang <hs1218.kang@samsung.com>

--- arch/arm/plat-samsung/include/plat/cpu.h
+++ arch/arm/plat-samsung/include/plat/cpu.h
@@ -136,6 +136,9 @@ IS_SAMSUNG_CPU(exynos5250, EXYNOS5250_CPU_ID, EXYNOS_CPU_MASK)
 # define soc_is_exynos5250()	0
 #endif
 
+#define EXYNOS5250_REV_0	(0x0)
+#define EXYNOS5250_REV_1_0	(0x10)
+
 #define IODESC_ENT(x) { (unsigned long)S3C24XX_VA_##x, __phys_to_pfn(S3C24XX_PA_##x), S3C24XX_SZ_##x, MT_DEVICE }
 
 #ifndef MHZ
