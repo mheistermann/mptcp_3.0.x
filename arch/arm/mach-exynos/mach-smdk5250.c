8bf6da1ddc7334aa6d1f4ce977811c6787664343
Yulgon Kim <yulgon.kim@samsung.com>
2012-03-21 10:26:52 +0900

    ARM: EXYNOS: Adds Host vbus control for EHCI & OHCI
    This patch add host vbus control for EHCI & OHCI.
    On board rev 0.1, GPX2[6] is allocated for 2.0 Host vbus.
    If USB_SWITCH does not used, This gpio is always High.
    
    Change-Id: I5b7cf9ea635e8f7d129f8e6271ecf15b888c59ff
    Signed-off-by: Yulgon Kim <yulgon.kim@samsung.com>

--- arch/arm/mach-exynos/mach-smdk5250.c
+++ arch/arm/mach-exynos/mach-smdk5250.c
@@ -2011,6 +2011,17 @@ static void __init smdk5250_ehci_init(void)
 {
 	struct s5p_ehci_platdata *pdata = &smdk5250_ehci_pdata;
 
+#ifndef CONFIG_USB_EXYNOS_SWITCH
+	if (samsung_rev() >= EXYNOS5250_REV_1_0) {
+		if (gpio_request_one(EXYNOS5_GPX2(6), GPIOF_OUT_INIT_HIGH,
+			"HOST_VBUS_CONTROL"))
+			printk(KERN_ERR "failed to request gpio_host_vbus\n");
+		else {
+			s3c_gpio_setpull(EXYNOS5_GPX2(6), S3C_GPIO_PULL_NONE);
+			gpio_free(EXYNOS5_GPX2(6));
+		}
+	}
+#endif
 	s5p_ehci_set_platdata(pdata);
 }
 #endif
