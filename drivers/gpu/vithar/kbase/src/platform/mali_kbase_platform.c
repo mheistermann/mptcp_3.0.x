f1235885149e67b702f27c19f74fc2aa3f6ad70a
WangjaeLee <lwj55378.lee@samsung.com>
2012-03-23 19:17:55 +0900

    gpu: vithar: Support EVT0 and EVT1
    Fix Build Error for EVT0
    
    Signed-off-by: WangjaeLee <lwj55378.lee@samsung.com>

--- drivers/gpu/vithar/kbase/src/platform/mali_kbase_platform.c
+++ drivers/gpu/vithar/kbase/src/platform/mali_kbase_platform.c
@@ -76,7 +76,7 @@ static int kbase_platform_power_clock_init(struct device *dev)
 
 	/* Turn on G3D clock */
 
-#if CONFIG_VITHAR_HWVER_R0P0
+#ifdef CONFIG_VITHAR_HWVER_R0P0
 	kbdev->sclk_g3d = clk_get(dev, "aclk_400");
 	if(IS_ERR(kbdev->sclk_g3d)) {
 		OSK_PRINT_ERROR(OSK_BASE_PM, "failed to clk_get [sclk_g3d]\n");
