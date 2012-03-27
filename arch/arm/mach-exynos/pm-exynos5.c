519a08bedb70750ae034ec20fb8cf59f9abcb8a3
Jun Kim <junkim@samsung.com>
2012-03-27 12:56:52 +0900

    ARM: EXYNOS: PM: fix wrong address of I2C_CFG register
    Change-Id: I3a0370fbd2678cdec45456adaae58cb81ebfca84
    Signed-off-by: Jun Kim <junkim@samsung.com>

--- arch/arm/mach-exynos/pm-exynos5.c
+++ arch/arm/mach-exynos/pm-exynos5.c
@@ -174,7 +174,7 @@ static struct sleep_save exynos5_core_save[] = {
 	SAVE_ITEM(S5P_VA_COMBINER_BASE + 0x060),
 	SAVE_ITEM(S5P_VA_COMBINER_BASE + 0x070),
 
-	SAVE_ITEM(S3C_VA_SYS + 0x134),
+	SAVE_ITEM(S3C_VA_SYS + 0x234),
 };
 
 void exynos5_cpu_suspend(void)
