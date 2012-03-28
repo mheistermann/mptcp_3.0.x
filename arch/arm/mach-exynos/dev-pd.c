2d27f66e9609db9a9a7ccd5f77cc9aa2f70532e4
Janghyuck Kim <janghyuck.kim@samsung.com>
2012-03-28 21:16:22 +0900

    ARM: EXYNOS: Add defence code for MFC 6.1 power domain
    This patch adds defence code for MFC 6.1 power domain.
    In Gaia evt0, MFC should not use power gating. This added code is
    working to avoid MFC power domain being turned off.
    
    Change-Id: I0a026d9c6a14c226101bd0ab192a3ea65aa6d020
    Signed-off-by: Janghyuck Kim <janghyuck.kim@samsung.com>

--- arch/arm/mach-exynos/dev-pd.c
+++ arch/arm/mach-exynos/dev-pd.c
@@ -92,6 +92,12 @@ int exynos_pd_disable(struct device *dev)
 		(pdata->base == EXYNOS5_ISP_CONFIGURATION))
 		return 0;
 #endif
+	/* Do not disable MFC power domain for EXYNOS5250 EVT0 */
+	if (soc_is_exynos5250() &&
+		(samsung_rev() < EXYNOS5250_REV_1_0) &&
+		(pdata->base == EXYNOS5_MFC_CONFIGURATION))
+		return 0;
+
 	/*
 	 * To ISP power domain off,
 	 * first, ISP_ARM power domain be off.
