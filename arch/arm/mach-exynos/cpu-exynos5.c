f187b9ccbb24a2bacee12af181cfccde48d2cbb5
Tonghun Hwang <thun.hwang@samsung.com>
2012-03-29 15:48:20 +0900

    ARM: EXYNOS: Mapping FIMCLITE2 VA and PA
    Mapping virture address and physical address of FIMCLITE2.
    
    Change-Id: I05838bcfeabda42f0f59a3431db9e975ad480341
    Signed-off-by: Tonghun Hwang <thun.hwang@samsung.com>

--- arch/arm/mach-exynos/cpu-exynos5.c
+++ arch/arm/mach-exynos/cpu-exynos5.c
@@ -152,6 +152,11 @@ static struct map_desc exynos5_iodesc[] __initdata = {
 		.length		= SZ_4K,
 		.type		= MT_DEVICE,
 	}, {
+		.virtual	= (unsigned long)S5P_VA_FIMCLITE2,
+		.pfn		= __phys_to_pfn(EXYNOS5_PA_FIMC_LITE2),
+		.length		= SZ_4K,
+		.type		= MT_DEVICE,
+	}, {
 		.virtual	= (unsigned long)S5P_VA_MIPICSI0,
 		.pfn		= __phys_to_pfn(EXYNOS5_PA_MIPI_CSIS0),
 		.length		= SZ_4K,
