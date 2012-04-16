0dacd0a48bea80d4fbda25cef58ea047ba98b956
Soohyun Kim <soohyuni.kim@samsung.com>
2012-04-16 14:19:52 +0900

    ARM: EXYNOS: Change gpio_input driver to gpio_keys driver for smdk5250 board.
    SMDK board has a problem. Device can not wakeup when gpio button is toggled
    very quickly. If the device wakeup time is longer than toggled POWER button
    time by user, device is not recognize one of the key event. It makes the
    android system not execute wakeup codes. This patch will resolve the problem.
    
    Change-Id: Ie3c9f13a6eb55be60cb600b09301889916c663e9
    Signed-off-by: Soohyun Kim <soohyuni.kim@samsung.com>

--- arch/arm/mach-exynos/board-smdk5250-input.c
+++ arch/arm/mach-exynos/board-smdk5250-input.c
@@ -9,7 +9,7 @@
 */
 
 #include <linux/gpio.h>
-#include <linux/gpio_event.h>
+#include <linux/gpio_keys.h>
 #include <linux/i2c.h>
 #include <linux/input.h>
 
@@ -21,63 +21,26 @@
 
 #include "board-smdk5250.h"
 
-static struct gpio_event_direct_entry smdk5250_keypad_key_map[] = {
+struct gpio_keys_button smdk5250_button[] = {
 	{
-		.gpio   = EXYNOS5_GPX0(0),
-		.code   = KEY_POWER,
-	}
-};
-
-static struct gpio_event_input_info smdk5250_keypad_key_info = {
-	.info.func              = gpio_event_input_func,
-	.info.no_suspend        = true,
-	.debounce_time.tv64	= 5 * NSEC_PER_MSEC,
-	.type                   = EV_KEY,
-	.keymap                 = smdk5250_keypad_key_map,
-	.keymap_size            = ARRAY_SIZE(smdk5250_keypad_key_map)
-};
-
-static struct gpio_event_info *smdk5250_input_info[] = {
-	&smdk5250_keypad_key_info.info,
-};
-
-static struct gpio_event_platform_data smdk5250_input_data = {
-	.names  = {
-		"smdk5250-keypad",
-		NULL,
-	},
-	.info           = smdk5250_input_info,
-	.info_count     = ARRAY_SIZE(smdk5250_input_info),
-};
-
-static struct platform_device smdk5250_input_device = {
-	.name   = GPIO_EVENT_DEV_NAME,
-	.id     = 0,
-	.dev    = {
-		.platform_data = &smdk5250_input_data,
+		.code = KEY_POWER,
+		.gpio = EXYNOS5_GPX0(0),
+		.active_low = 1,
+		.wakeup = 1,
 	},
 };
 
-static void __init smdk5250_gpio_power_init(void)
-{
-	int err = 0;
-
-	err = gpio_request_one(EXYNOS5_GPX0(0), 0, "GPX0");
-	if (err) {
-		printk(KERN_ERR "failed to request GPX0 for "
-				"suspend/resume control\n");
-		return;
-	}
-	s3c_gpio_setpull(EXYNOS5_GPX0(0), S3C_GPIO_PULL_NONE);
-
-	gpio_free(EXYNOS5_GPX0(0));
-}
-
-#ifdef CONFIG_WAKEUP_ASSIST
-static struct platform_device wakeup_assist_device = {
-	.name = "wakeup_assist",
+struct gpio_keys_platform_data smdk5250_gpiokeys_platform_data = {
+	smdk5250_button,
+	ARRAY_SIZE(smdk5250_button),
 };
-#endif
+
+static struct platform_device smdk5250_gpio_keys = {
+	.name	= "gpio-keys",
+	.dev	= {
+		.platform_data = &smdk5250_gpiokeys_platform_data,
+	},
+};
 
 struct egalax_i2c_platform_data {
 	unsigned int gpio_int;
@@ -99,18 +62,13 @@ static struct i2c_board_info i2c_devs7[] __initdata = {
 
 static struct platform_device *smdk5250_input_devices[] __initdata = {
 	&s3c_device_i2c7,
-	&smdk5250_input_device,
-#ifdef CONFIG_WAKEUP_ASSIST
-	&wakeup_assist_device,
-#endif
+	&smdk5250_gpio_keys,
 };
 
 void __init exynos5_smdk5250_input_init(void)
 {
 	s3c_i2c7_set_platdata(NULL);
 	i2c_register_board_info(7, i2c_devs7, ARRAY_SIZE(i2c_devs7));
-
-	smdk5250_gpio_power_init();
 
 	platform_add_devices(smdk5250_input_devices,
 			ARRAY_SIZE(smdk5250_input_devices));
