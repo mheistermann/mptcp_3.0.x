5932c87872728c175d8c733c9ffc73b1d0915c85
Taekgyun Ko <taeggyun.ko@samsung.com>
2012-04-19 16:58:51 +0900

    ARM: EXYNOS: HS-I2C channel 0 gpio configuration
    Set GPB3[0] as HS-I2C channel 0 SDA.
    Set GPB3[1] as HS-I2C channel 0 SCL.
    
    Change-Id: Id846fd13eea2b2df300a8b212f74e78505f9bdcd
    Signed-off-by: Taekgyun Ko <taeggyun.ko@samsung.com>

---
+++ arch/arm/mach-exynos/setup-hs-i2c0.c
@@ -0,0 +1,21 @@
+/*
+ * linux/arch/arm/mach-exynos/setup-hs-i2c0.c
+ *
+ * Copyright (C) 2012 Samsung Electronics Co., Ltd.
+ *
+ * HS-I2C0 GPIO configuration.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+*/
+
+#include <linux/gpio.h>
+#include <plat/gpio-cfg.h>
+#include <plat/cpu.h>
+
+void exynos5_hs_i2c0_cfg_gpio(struct platform_device *dev)
+{
+	s3c_gpio_cfgall_range(EXYNOS5_GPB3(0), 2,
+			S3C_GPIO_SFN(4), S3C_GPIO_PULL_UP);
+}
