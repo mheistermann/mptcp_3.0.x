ab29c7a79561b7b542ffdf3197b7f17cfb3ff8ea
Sangsu Park <sangsu4u.park@samsung.com>
2012-05-02 19:29:39 +0900

    GPIO: SAMSUNG: add GPIO Map for exynos5410
    This patch add definitions for exynos5410 GPIO and
    register exynos5410 GPIO map from GPIO_LB to GPIO_C2C.
    
    Change-Id: I15ed8a65a580f80b1eddbc8673b04bcc2316b242
    Signed-off-by: Sangsu Park <sangsu4u.park@samsung.com>

--- drivers/gpio/gpio-exynos5.c
+++ drivers/gpio/gpio-exynos5.c
@@ -65,6 +65,7 @@ static struct s3c_gpio_cfg gpio_cfg_noint = {
  * Note: The initialization of 'base' member of s3c_gpio_chip structure
  * uses the above macro and depends on the banks being listed in order here.
  */
+#if defined(CONFIG_MACH_SMDK5250)
 static struct s3c_gpio_chip exynos5_gpio_common_4bit[] = {
 	{
 		.base	= S5P_VA_GPIO1,
@@ -406,6 +407,436 @@ static struct s3c_gpio_chip exynos5_gpio_common_4bit[] = {
 		},
 	},
 };
+#else /* CONFIG_MACH_SMDK5410 */
+static struct s3c_gpio_chip exynos5_gpio_common_4bit[] = {
+	{
+		.base	= S5P_VA_GPIO1,
+		.eint_offset = 0x0,
+		.group	= 0,
+		.chip	= {
+			.base	= EXYNOS5_GPJ0(0),
+			.ngpio	= EXYNOS5_GPIO_J0_NR,
+			.label	= "GPJ0",
+		},
+	}, {
+		.base	= (S5P_VA_GPIO1 + 0x20),
+		.eint_offset = 0x4,
+		.group	= 1,
+		.chip	= {
+			.base	= EXYNOS5_GPJ1(0),
+			.ngpio	= EXYNOS5_GPIO_J1_NR,
+			.label	= "GPJ1",
+		},
+	}, {
+		.base	= (S5P_VA_GPIO1 + 0x40),
+		.eint_offset = 0x8,
+		.group	= 2,
+		.chip	= {
+			.base	= EXYNOS5_GPJ2(0),
+			.ngpio	= EXYNOS5_GPIO_J2_NR,
+			.label	= "GPJ2",
+		},
+	}, {
+		.base	= (S5P_VA_GPIO1 + 0x60),
+		.eint_offset = 0xC,
+		.group	= 3,
+		.chip	= {
+			.base	= EXYNOS5_GPJ3(0),
+			.ngpio	= EXYNOS5_GPIO_J3_NR,
+			.label	= "GPJ3",
+		},
+	}, {
+		.base	= (S5P_VA_GPIO1 + 0x80),
+		.eint_offset = 0x10,
+		.group	= 4,
+		.chip	= {
+			.base	= EXYNOS5_GPJ4(0),
+			.ngpio	= EXYNOS5_GPIO_J4_NR,
+			.label	= "GPJ4",
+		},
+	}, {
+		.base	= (S5P_VA_GPIO1 + 0xA0),
+		.eint_offset = 0x14,
+		.group	= 5,
+		.chip	= {
+			.base	= EXYNOS5_GPK0(0),
+			.ngpio	= EXYNOS5_GPIO_K0_NR,
+			.label	= "GPK0",
+		},
+	}, {
+		.base	= (S5P_VA_GPIO1 + 0xC0),
+		.eint_offset = 0x18,
+		.group	= 6,
+		.chip	= {
+			.base	= EXYNOS5_GPK1(0),
+			.ngpio	= EXYNOS5_GPIO_K1_NR,
+			.label	= "GPK1",
+		},
+	}, {
+		.base	= (S5P_VA_GPIO1 + 0xE0),
+		.eint_offset = 0x1C,
+		.group	= 7,
+		.chip	= {
+			.base	= EXYNOS5_GPK2(0),
+			.ngpio	= EXYNOS5_GPIO_K2_NR,
+			.label	= "GPK2",
+		},
+	}, {
+		.base	= (S5P_VA_GPIO1 + 0x100),
+		.eint_offset = 0x20,
+		.group	= 8,
+		.chip	= {
+			.base	= EXYNOS5_GPK3(0),
+			.ngpio	= EXYNOS5_GPIO_K3_NR,
+			.label	= "GPK3",
+		},
+	}, {
+		.base	= S5P_VA_GPIO2,
+		.eint_offset = 0x0,
+		.group	= 9,
+		.chip	= {
+			.base	= EXYNOS5_GPA0(0),
+			.ngpio	= EXYNOS5_GPIO_A0_NR,
+			.label	= "GPA0",
+		},
+	}, {
+		.base	= (S5P_VA_GPIO2 + 0x20),
+		.eint_offset = 0x4,
+		.group	= 10,
+		.chip	= {
+			.base	= EXYNOS5_GPA1(0),
+			.ngpio	= EXYNOS5_GPIO_A1_NR,
+			.label	= "GPA1",
+		},
+	}, {
+		.base	= (S5P_VA_GPIO2 + 0x40),
+		.eint_offset = 0x8,
+		.group	= 11,
+		.chip	= {
+			.base	= EXYNOS5_GPA2(0),
+			.ngpio	= EXYNOS5_GPIO_A2_NR,
+			.label	= "GPA2",
+		},
+	}, {
+		.base	= (S5P_VA_GPIO2 + 0x60),
+		.eint_offset = 0xC,
+		.group	= 12,
+		.chip	= {
+			.base	= EXYNOS5_GPB0(0),
+			.ngpio	= EXYNOS5_GPIO_B0_NR,
+			.label	= "GPB0",
+		},
+	}, {
+		.base	= (S5P_VA_GPIO2 + 0x80),
+		.eint_offset = 0x10,
+		.group	= 13,
+		.chip	= {
+			.base	= EXYNOS5_GPB1(0),
+			.ngpio	= EXYNOS5_GPIO_B1_NR,
+			.label	= "GPB1",
+		},
+	}, {
+		.base	= (S5P_VA_GPIO2 + 0xA0),
+		.eint_offset = 0x14,
+		.group	= 14,
+		.chip	= {
+			.base	= EXYNOS5_GPB2(0),
+			.ngpio	= EXYNOS5_GPIO_B2_NR,
+			.label	= "GPB2",
+		},
+	}, {
+		.base	= (S5P_VA_GPIO2 + 0xC0),
+		.eint_offset = 0x18,
+		.group	= 15,
+		.chip	= {
+			.base	= EXYNOS5_GPB3(0),
+			.ngpio	= EXYNOS5_GPIO_B3_NR,
+			.label	= "GPB3",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO2 + 0xE0),
+		.eint_offset = 0x1C,
+		.group	= 16,
+		.chip	= {
+			.base	= EXYNOS5_GPC0(0),
+			.ngpio	= EXYNOS5_GPIO_C0_NR,
+			.label	= "GPC0",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO2 + 0x100),
+		.eint_offset = 0x20,
+		.group	= 17,
+		.chip	= {
+			.base	= EXYNOS5_GPC3(0),
+			.ngpio	= EXYNOS5_GPIO_C3_NR,
+			.label	= "GPC3",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO2 + 0x120),
+		.eint_offset = 0x24,
+		.group	= 18,
+		.chip	= {
+			.base	= EXYNOS5_GPC1(0),
+			.ngpio	= EXYNOS5_GPIO_C1_NR,
+			.label	= "GPC1",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO2 + 0x140),
+		.eint_offset = 0x28,
+		.group	= 19,
+		.chip	= {
+			.base	= EXYNOS5_GPC2(0),
+			.ngpio	= EXYNOS5_GPIO_C2_NR,
+			.label	= "GPC2",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO2 + 0x160),
+		.config	= &gpio_cfg_noint,
+		.chip	= {
+			.base	= EXYNOS5_GPM5(0),
+			.ngpio	= EXYNOS5_GPIO_M5_NR,
+			.label	= "GPM5",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO2 + 0x180),
+		.eint_offset = 0x2C,
+		.group	= 20,
+		.chip	= {
+			.base	= EXYNOS5_GPD1(0),
+			.ngpio	= EXYNOS5_GPIO_D1_NR,
+			.label	= "GPD1",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO2 + 0x1A0),
+		.eint_offset = 0x30,
+		.group	= 21,
+		.chip	= {
+			.base	= EXYNOS5_GPE0(0),
+			.ngpio	= EXYNOS5_GPIO_E0_NR,
+			.label	= "GPE0",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO2 + 0x1C0),
+		.eint_offset = 0x34,
+		.group	= 22,
+		.chip	= {
+			.base	= EXYNOS5_GPE1(0),
+			.ngpio	= EXYNOS5_GPIO_E1_NR,
+			.label	= "GPE1",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO2 + 0x1E0),
+		.eint_offset = 0x38,
+		.group	= 23,
+		.chip	= {
+			.base	= EXYNOS5_GPF0(0),
+			.ngpio	= EXYNOS5_GPIO_F0_NR,
+			.label	= "GPF0",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO2 + 0x200),
+		.eint_offset = 0x3C,
+		.group	= 24,
+		.chip	= {
+			.base	= EXYNOS5_GPF1(0),
+			.ngpio	= EXYNOS5_GPIO_F1_NR,
+			.label	= "GPF1",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO2 + 0x220),
+		.eint_offset = 0x40,
+		.group	= 25,
+		.chip	= {
+			.base	= EXYNOS5_GPG0(0),
+			.ngpio	= EXYNOS5_GPIO_G0_NR,
+			.label	= "GPG0",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO2 + 0x240),
+		.eint_offset = 0x44,
+		.group	= 26,
+		.chip	= {
+			.base	= EXYNOS5_GPG1(0),
+			.ngpio	= EXYNOS5_GPIO_G1_NR,
+			.label	= "GPG1",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO2 + 0x260),
+		.eint_offset = 0x48,
+		.group	= 27,
+		.chip	= {
+			.base	= EXYNOS5_GPG2(0),
+			.ngpio	= EXYNOS5_GPIO_G2_NR,
+			.label	= "GPG2",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO2 + 0x280),
+		.eint_offset = 0x4C,
+		.group	= 28,
+		.chip	= {
+			.base	= EXYNOS5_GPH0(0),
+			.ngpio	= EXYNOS5_GPIO_H0_NR,
+			.label	= "GPH0",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO2 + 0x2A0),
+		.eint_offset = 0x50,
+		.group	= 29,
+		.chip	= {
+			.base	= EXYNOS5_GPH1(0),
+			.ngpio	= EXYNOS5_GPIO_H1_NR,
+			.label	= "GPH1",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO2 + 0x2C0),
+		.config	= &gpio_cfg_noint,
+		.chip	= {
+			.base	= EXYNOS5_GPM7(0),
+			.ngpio	= EXYNOS5_GPIO_M7_NR,
+			.label	= "GPM7",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO2 + 0x2E0),
+		.config	= &gpio_cfg_noint,
+		.chip	= {
+			.base	= EXYNOS5_GPY0(0),
+			.ngpio	= EXYNOS5_GPIO_Y0_NR,
+			.label	= "GPY0",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO2 + 0x300),
+		.config	= &gpio_cfg_noint,
+		.chip	= {
+			.base	= EXYNOS5_GPY1(0),
+			.ngpio	= EXYNOS5_GPIO_Y1_NR,
+			.label	= "GPY1",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO2 + 0x320),
+		.config	= &gpio_cfg_noint,
+		.chip	= {
+			.base	= EXYNOS5_GPY2(0),
+			.ngpio	= EXYNOS5_GPIO_Y2_NR,
+			.label	= "GPY2",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO2 + 0x340),
+		.config	= &gpio_cfg_noint,
+		.chip	= {
+			.base	= EXYNOS5_GPY3(0),
+			.ngpio	= EXYNOS5_GPIO_Y3_NR,
+			.label	= "GPY3",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO2 + 0x360),
+		.config	= &gpio_cfg_noint,
+		.chip	= {
+			.base	= EXYNOS5_GPY4(0),
+			.ngpio	= EXYNOS5_GPIO_Y4_NR,
+			.label	= "GPY4",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO2 + 0x380),
+		.config	= &gpio_cfg_noint,
+		.chip	= {
+			.base	= EXYNOS5_GPY5(0),
+			.ngpio	= EXYNOS5_GPIO_Y5_NR,
+			.label	= "GPY5",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO2 + 0x3A0),
+		.config	= &gpio_cfg_noint,
+		.chip	= {
+			.base	= EXYNOS5_GPY6(0),
+			.ngpio	= EXYNOS5_GPIO_Y6_NR,
+			.label	= "GPY6",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO2 + 0x3C0),
+		.config	= &gpio_cfg_noint,
+		.chip	= {
+			.base	= EXYNOS5_GPY7(0),
+			.ngpio	= EXYNOS5_GPIO_Y7_NR,
+			.label	= "GPY7",
+		},
+	}, {
+		.base	= (S5P_VA_GPIO2 + 0xC00),
+		.config	= &gpio_cfg_noint,
+		.irq_base = IRQ_EINT(0),
+		.chip	= {
+			.base	= EXYNOS5_GPX0(0),
+			.ngpio	= EXYNOS5_GPIO_X0_NR,
+			.label	= "GPX0",
+			.to_irq	= samsung_gpiolib_to_irq,
+		},
+	}, {
+		.base	= (S5P_VA_GPIO2 + 0xC20),
+		.config	= &gpio_cfg_noint,
+		.irq_base = IRQ_EINT(8),
+		.chip	= {
+			.base	= EXYNOS5_GPX1(0),
+			.ngpio	= EXYNOS5_GPIO_X1_NR,
+			.label	= "GPX1",
+			.to_irq	= samsung_gpiolib_to_irq,
+		},
+	}, {
+		.base	= (S5P_VA_GPIO2 + 0xC40),
+		.config	= &gpio_cfg_noint,
+		.irq_base = IRQ_EINT(16),
+		.chip	= {
+			.base	= EXYNOS5_GPX2(0),
+			.ngpio	= EXYNOS5_GPIO_X2_NR,
+			.label	= "GPX2",
+			.to_irq	= samsung_gpiolib_to_irq,
+		},
+	}, {
+		.base	= (S5P_VA_GPIO2 + 0xC60),
+		.config	= &gpio_cfg_noint,
+		.irq_base = IRQ_EINT(24),
+		.chip	= {
+			.base	= EXYNOS5_GPX3(0),
+			.ngpio	= EXYNOS5_GPIO_X3_NR,
+			.label	= "GPX3",
+			.to_irq	= samsung_gpiolib_to_irq,
+		},
+	}, {
+		.base   = S5P_VA_GPIO3,
+		.chip	= {
+			.base	= EXYNOS5_GPV0(0),
+			.ngpio	= EXYNOS5_GPIO_V0_NR,
+			.label	= "GPV0",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO3 + 0x20),
+		.chip	= {
+			.base	= EXYNOS5_GPV1(0),
+			.ngpio	= EXYNOS5_GPIO_V1_NR,
+			.label	= "GPV1",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO3 + 0x60),
+		.chip	= {
+			.base	= EXYNOS5_GPV2(0),
+			.ngpio	= EXYNOS5_GPIO_V2_NR,
+			.label	= "GPV2",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO3 + 0x80),
+		.chip	= {
+			.base	= EXYNOS5_GPV3(0),
+			.ngpio	= EXYNOS5_GPIO_V3_NR,
+			.label	= "GPV3",
+		},
+	}, {
+		.base   = (S5P_VA_GPIO3 + 0xC0),
+		.chip	= {
+			.base	= EXYNOS5_GPV4(0),
+			.ngpio	= EXYNOS5_GPIO_V4_NR,
+			.label	= "GPV4",
+		},
+	},
+};
+#endif
 
 static struct s3c_gpio_chip exynos5_gpio_no_pm_4bit[] = {
 {
