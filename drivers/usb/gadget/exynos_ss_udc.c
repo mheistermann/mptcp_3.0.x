a857d0b02b3cdf16374a28e3ea8233abf4c5f228
Yulgon Kim <yulgon.kim@samsung.com>
2012-05-09 16:28:15 +0900

    usb: exynos_ss_udc: support USB LPM in high speed
    This patch support LPM(Lower Power Management) in high speed.
    This is the item of USB 3.0 Compliance test.
    USB CV test - TD 9.21:LPM L1 Suspend Resume Test
    
    Change-Id: I3f1dc173e3b652aeb97de582f501350a0271869a
    Signed-off-by: Yulgon Kim <yulgon.kim@samsung.com>

--- drivers/usb/gadget/exynos_ss_udc.c
+++ drivers/usb/gadget/exynos_ss_udc.c
@@ -2569,7 +2569,9 @@ static void exynos_ss_udc_init(struct exynos_ss_udc *udc)
 	writel(EXYNOS_USB3_EVENT_BUFF_BSIZE,
 	       udc->regs + EXYNOS_USB3_GEVNTSIZ(0));
 
-	writel(EXYNOS_USB3_DCFG_NumP(1) | EXYNOS_USB3_DCFG_PerFrInt(2) |
+	writel(EXYNOS_USB3_DCFG_NumP(1) |
+	       EXYNOS_USB3_DCFG_PerFrInt(2) |
+	       EXYNOS_USB3_DCFG_LPMCap |
 #if defined(CONFIG_USB_GADGET_EXYNOS_SS_UDC_SSMODE)
 	       EXYNOS_USB3_DCFG_DevSpd(4),
 #else
