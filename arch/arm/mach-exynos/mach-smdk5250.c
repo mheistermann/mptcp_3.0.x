e8c2c56dc98eb10b2d41a01db80da6f613f5af9c
Dongsu Ha <dsfine.ha@samsung.com>
2012-08-09 14:13:48 +0900

    ARM: EXYNOS: Modify the code of I2C3 irq
    i2c3 irq is using coasia touch screen.
    However smdk5250 support many different type board of Rev.
    This patch distinguish irq number according to smdk5250 Rev.
    
    Change-Id: Ib2d75abb3edaa0700920efb272c1331f4dba0ac3
    Signed-off-by: Dongsu Ha <dsfine.ha@samsung.com>

--- arch/arm/mach-exynos/mach-smdk5250.c
+++ arch/arm/mach-exynos/mach-smdk5250.c
@@ -1133,7 +1133,7 @@ static void __init smdk5250_machine_init(void)
 	if (samsung_board_rev_is_0_0())
 		i2c_devs3[0].irq = IRQ_EINT(21);
 	else
-		i2c_devs3[0].irq = IRQ_EINT(18);
+		i2c_devs3[0].irq = IRQ_EINT(21);
 	i2c_register_board_info(3, i2c_devs3, ARRAY_SIZE(i2c_devs3));
 
 	s3c_i2c4_set_platdata(NULL);
